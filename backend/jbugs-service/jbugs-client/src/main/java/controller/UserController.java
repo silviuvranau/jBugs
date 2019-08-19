52fd5bd0d23e700702de7638ad07ee7d271ee8d4
Radu Ungureanu <Radu.UNGUREANU@student.utcluj.ro>
2019-08-19 17:26:21 +0300

    updated User RestController

--- backend/jbugs-service/jbugs-client/src/main/java/controller/UserController.java
+++ backend/jbugs-service/jbugs-client/src/main/java/controller/UserController.java
@@ -8,6 +8,7 @@ import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.util.List;
 
 @Path("/users")
 public class UserController {
@@ -19,5 +20,10 @@ public class UserController {
     @Path("{userId}")
     public UserDTO getUserById(@PathParam("userId") Integer userId) {
         return userManager.findAUser(userId);
+    }
+
+    @GET
+    public List<UserDTO> getAllUsers(){
+        return userManager.findAllUsers();
     }
 }
