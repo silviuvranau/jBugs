e5d142e5b3234cb9244c69dc543ef54148259327
Stefan Lelutiu <liverpoolanfield03641@gmail.com>
2019-08-21 17:48:31 +0300

    Backend la login form

--- backend/jbugs-service/jbugs-client/src/main/java/controller/UserController.java
+++ backend/jbugs-service/jbugs-client/src/main/java/controller/UserController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import exceptions.BusinessException;
+import ro.msg.edu.jbugs.dto.CredentialDTO;
 import ro.msg.edu.jbugs.dto.UserDTO;
 import ro.msg.edu.jbugs.managers.implementations.UserManager;
 import ro.msg.edu.jbugs.managers.interfaces.UserManagerRemote;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/users")
@@ -27,7 +31,13 @@ public class UserController {
     }
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     public UserDTO createUser(UserDTO userDTO) {
         return userManager.insertUser(userDTO);
+    }
+    @POST
+    @Path("/auth")
+    public UserDTO login(CredentialDTO credentialDTO) throws BusinessException {
+        return userManager.login(credentialDTO.getUsername(),credentialDTO.getPassword());
     }
 }
