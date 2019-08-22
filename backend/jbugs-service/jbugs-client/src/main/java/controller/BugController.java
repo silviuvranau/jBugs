b523a52b19604ab85bfb03403c7ab4bc0757deb2
Stefania Pricop <pricop.stefania98@yahoo.ro>
2019-08-22 10:41:14 +0300

    Merge branch 'jbg-001-add/revoke_rights' of https://github.com/silviuvranau/jBugs into jbg-001-add-revoke-rights-finale
    # Conflicts:
    #	backend/jbugs-service/jbugs-business/src/test/java/ro/msg/edu/jbugs/managers/implementations/RoleManagerTest.java
    #	backend/jbugs-service/jbugs-client/src/main/java/controller/RoleController.java
    #	backend/jbugs-service/jbugs-persistence/src/main/java/dao/RoleDao.java

---
+++ backend/jbugs-service/jbugs-client/src/main/java/controller/BugController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import ro.msg.edu.jbugs.dto.BugDTO;
+import ro.msg.edu.jbugs.managers.interfaces.BugManagerRemote;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
+
+/**
+ * Document me.
+ *
+ * @author msg systems AG; Pricop Stefania.
+ * @since 19.1.2
+ */
+@Path("/bugs")
+@Produces("application/json")
+public class BugController {
+    @EJB
+    BugManagerRemote bugManagerRemote;
+
+
+    @GET
+    public List<BugDTO> getAllBugs() {
+        return bugManagerRemote.findAllBugs();
+    }
+}
